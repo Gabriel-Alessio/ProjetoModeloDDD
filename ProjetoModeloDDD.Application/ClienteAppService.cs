6183d701e3caad90afc68733787e1071c720f8f2
gabryelalessio <gabryelalessio@gmail.com>
2022-05-26 14:14:47 -0300

    Fix

--- ProjetoModeloDDD.Application/ClienteAppService.cs
+++ ProjetoModeloDDD.Application/ClienteAppService.cs
@@ -1,11 +1,7 @@
 ﻿using ProjetoModeloDDD.Application.Interface;
 using ProjetoModeloDDD.Domain.Entities;
 using ProjetoModeloDDD.Domain.Interfaces.Services;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjetoModeloDDD.Application
 {
--- ProjetoModeloDDD.MVC/Controllers/HomeController.cs
+++ ProjetoModeloDDD.MVC/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProjetoModeloDDD.MVC.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ProjetoModeloDDD.MVC.Controllers
 {
